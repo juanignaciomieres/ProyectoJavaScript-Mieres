39d479fd2c9b25356dbc488420e3f2a209c8da65
Juan Ignacio Mieres <juanignaciomieres@gmail.com>
2024-11-22 12:20:44 -0300

    JAVA-Clase1

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+console.log("El zurdo te da la bienvenida")
+
 const titulo = document.getElementById ("titulo");
 
 titulo.textContent = "Hola Zorras";
 
 titulo.style.color = "blue";
 titulo.style.fontSize = "50px";
+
+console
